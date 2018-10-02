dde7f92a4c777f44b1b59866a3b02415c7ebefd6
james chalmers <james.chalmers184@gmail.com>
2018-10-02 10:47:04 +0100

    minor changes

--- CoreCSharp/CoreCSharpSamples/MathLib/Calculator.cs
+++ CoreCSharp/CoreCSharpSamples/MathLib/Calculator.cs
@@ -1,7 +1,18 @@
-﻿namespace MathLib
+﻿//Mathlib.cs
+namespace MathLib
 {
+    /// <summary>
+    /// Wrox.MathLib.Calculator class.
+    /// Provides a method to add two doubles
+    /// </summary>
     public class Calculator
     {
+        /// <summary>
+        /// The Add method allows us to add two doubles
+        /// </summary>
+        /// <param name="x">First number to add</param>
+        /// <param name="y">Second number to add</param>
+        /// <returns>Result of the addition (double)</returns>
         public static double Add(double x, double y) => x + y;
 
         public static double Subtract(double x, double y) => x - y;
